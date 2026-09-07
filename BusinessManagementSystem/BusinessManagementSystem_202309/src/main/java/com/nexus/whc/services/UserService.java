9b5679c1959b7128c0f4950beabe02bf25437f2e
r_mori <r_mori@nexusnt2015.onmicrosoft.com>
2026-09-07 15:59:29 +0900

    プッシュお試し

---
+++ BusinessManagementSystem/BusinessManagementSystem_202309/src/main/java/com/nexus/whc/services/UserService.java
@@ -0,0 +1,49 @@
+ 
+package com.nexus.whc.services;
+ 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nexus.whc.form.UserForm;
+import com.nexus.whc.repository.UserRepository;
+ 
+/* UserService.java
+* 
+* UserServiceクラス
+*/
+@Service
+public class UserService {
+	private final UserRepository userRepository;
+ 
+	@Autowired
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+ 
+	/* ユーザー一覧検索*/
+	public List<Map<String, Object>> searchList(
+			String userId,
+			String userName,
+			String authId,
+			String mailAddress) {
+ 
+		return userRepository.searchList(
+				userId,
+				userName,
+				authId,
+				mailAddress);
+	}
+ 
+	/*ユーザー登録*/
+	public int registUser(UserForm userForm) {
+		return userRepository.registUser(userForm);
+	}
+ 
+	/*ユーザー削除*/
+	public void deleteUser(int seqId) {
+		userRepository.deleteUser(seqId);
+	}
+}
