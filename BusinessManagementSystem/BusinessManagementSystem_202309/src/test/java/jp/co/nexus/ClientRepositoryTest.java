30145d9f97ec03907b7025073b11393ebeb22dee
r_mori <r_mori@nexusnt2015.onmicrosoft.com>
2026-09-17 17:21:48 +0900

    Client関連テストクラス作成

---
+++ BusinessManagementSystem/BusinessManagementSystem_202309/src/test/java/jp/co/nexus/ClientRepositoryTest.java
@@ -0,0 +1,14 @@
+package jp.co.nexus;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClientRepositoryTest {
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}
